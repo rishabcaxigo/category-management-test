8dfaef7df323b929f4c39dc82e6bff5e9ab78713
Rishab Puthiyedath <76944171+rishabcaxigo@users.noreply.github.com>
2022-08-24 23:57:10 +0530

    Add files via upload

---
+++ external/js/tree.js
@@ -0,0 +1,103 @@
+$.fn.extend({
+	treed: function (o) {
+		var openedClass = "glyphicon-minus-sign";
+		var closedClass = "glyphicon-plus-sign";
+
+		if (typeof o != "undefined") {
+			if (typeof o.openedClass != "undefined") {
+				openedClass = o.openedClass;
+			}
+			if (typeof o.closedClass != "undefined") {
+				closedClass = o.closedClass;
+			}
+		}
+
+		//initialize each of the top levels
+		var tree = $(this);
+		$("#Expand").on("click", function () {
+			if ($(this).html() == "Expand All") {
+				tree.find(".indicator").each(function () {
+					if ($(this).hasClass(closedClass) == true) {
+						$(this).closest("li").click();
+					}
+				});
+				$(this).html("Collapse All");
+			} else {
+				tree.find(".indicator").each(function () {
+					if ($(this).hasClass(openedClass) == true) {
+						$(this).closest("li").click();
+					}
+				});
+				$(this).html("Expand All");
+			}
+
+			//fire event from the dynamically added icon
+		});
+
+		tree.addClass("tree");
+		tree
+			.find("li")
+			.has("ul")
+			.each(function () {
+				var branch = $(this); //li with children ul
+				branch.prepend(
+					"<i class='indicator glyphicon " + closedClass + "'></i>"
+				);
+				branch.addClass("branch");
+				branch.on("click", function (e) {
+					if (this == e.target) {
+						var icon = $(this).children("i:first");
+						icon.toggleClass(openedClass + " " + closedClass);
+						$(this).children().children().toggle();
+					}
+				});
+				branch.children().children().toggle();
+			});
+		//fire event from the dynamically added icon
+		tree.find(".branch .indicator").each(function () {
+			$(this).on("click", function () {
+				$(this).closest("li").click();
+			});
+		});
+		//fire event to open branch if the li contains an anchor instead of text
+		tree.find(".branch>a").each(function () {
+			$(this).on("click", function (e) {
+				// $(this).closest("li").click();
+				e.preventDefault();
+			});
+		});
+		tree.find(".branch>a").each(function () {
+			$(this).on("dblclick", function (e) {
+				location.assign($(this).attr("href"));
+			});
+		});
+		
+		//fire event to open branch if the li contains a button instead of text
+		// tree.find(".branch>button").each(function () {
+		// 	$(this).on("click", function (e) {
+		// 		$(this).closest("li").click();
+		// 		e.preventDefault();
+		// 	});
+		// });
+	},
+});
+
+//Initialization of treeviews
+
+$("#tree1").treed();
+
+function imgError(image) {
+	image.onerror = "";
+	image.src = "/external/images/noimage.jpg";
+	return true;
+}
+
+// $("#tree2").treed({
+// 	openedClass: "glyphicon-folder-open",
+// 	closedClass: "glyphicon-folder-close",
+// });
+
+// $("#tree3").treed({
+// 	openedClass: "glyphicon-chevron-right",
+// 	closedClass: "glyphicon-chevron-down",
+// });
